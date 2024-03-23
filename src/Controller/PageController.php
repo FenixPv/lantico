e6cb910e3139129066e758b3eca400079258c24d
Pavel Klochkov <pv.klochkov@yandex.ru>
2024-03-23 13:12:15 +0300

    create page show

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Page;
+use App\Repository\PageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class PageController extends AbstractController
 {
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Environment $twig, PageRepository $pageRepository): Response
     {
-        return $this->render('page/index.html.twig', [
-            'controller_name' => 'PageController',
-        ]);
+        return new Response($twig->render('page/index.html.twig', [
+            'pages' => $pageRepository->findAll(),
+        ]));
+    }
+
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
+    #[Route('/{id}', name: 'page')]
+    public function show(Environment $twig, Page $page): Response
+    {
+        return new Response($twig->render('page/show.html.twig', [
+            'page' => $page,
+        ]));
     }
 }
