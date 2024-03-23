2870dea145c0a6604c876f6b213f307622791b17
Pavel Klochkov <pv.klochkov@yandex.ru>
2024-03-23 15:47:02 +0300

    refactoring PageController.php

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -7,36 +7,22 @@ use App\Repository\PageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Twig\Environment;
-use Twig\Error\LoaderError;
-use Twig\Error\RuntimeError;
-use Twig\Error\SyntaxError;
 
 class PageController extends AbstractController
 {
-    /**
-     * @throws SyntaxError
-     * @throws RuntimeError
-     * @throws LoaderError
-     */
     #[Route('/', name: 'homepage')]
-    public function index(Environment $twig, PageRepository $pageRepository): Response
+    public function index(PageRepository $pageRepository): Response
     {
-        return new Response($twig->render('page/index.html.twig', [
+        return $this->render('page/index.html.twig', [
             'pages' => $pageRepository->findAll(),
-        ]));
+        ]);
     }
 
-    /**
-     * @throws SyntaxError
-     * @throws RuntimeError
-     * @throws LoaderError
-     */
     #[Route('/{id}', name: 'page')]
-    public function show(Environment $twig, Page $page): Response
+    public function show(Page $page): Response
     {
-        return new Response($twig->render('page/show.html.twig', [
+        return $this->render('page/show.html.twig', [
             'page' => $page,
-        ]));
+        ]);
     }
 }
