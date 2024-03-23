38b25a3a8d01f454489f909de8b266784ef5a807
Pavel Klochkov <pv.klochkov@yandex.ru>
2024-03-23 12:34:39 +0300

    add CRUD page cpanel

--- src/Controller/Cpanel/CpanelController.php
+++ src/Controller/Cpanel/CpanelController.php
@@ -2,34 +2,29 @@
 
 namespace App\Controller\Cpanel;
 
+use App\Entity\Page;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class CpanelController extends AbstractDashboardController
 {
+    /**
+     * @throws NotFoundExceptionInterface
+     * @throws ContainerExceptionInterface
+     */
     #[Route('/cpanel', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
+        $url = $routeBuilder->setController(PageCrudController::class)->generateUrl();
+        return $this->redirect($url);
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -40,7 +35,8 @@ class CpanelController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+
+        yield MenuItem::linkToCrud('Pages', 'fas fa-map-marker-alt', Page::class);
+
     }
 }
---
+++ src/Controller/Cpanel/PageCrudController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Cpanel;
+
+use App\Entity\Page;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class PageCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Page::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield TextField::new('title', 'Title');
+        yield TextField::new('slug', 'Slug');
+        yield TextareaField::new('description', 'Описание');
+        yield ImageField::new('cover', 'Обложка')
+            ->setUploadDir('public/page/images');
+        yield TextEditorField::new('Body', 'Тело страницы');
+
+        $createdAt = DateTimeField::new('createdAt')->setFormTypeOptions(options: [
+            'years' => range(date('Y'), end: date(format: 'Y') + 5),
+            'widget' => 'single_text',
+        ]);
+
+        if (Crud::PAGE_EDIT === $pageName) {
+            yield $createdAt->setFormTypeOption('disabled', true);
+        } else {
+            yield $createdAt;
+        }
+    }
+
+}
