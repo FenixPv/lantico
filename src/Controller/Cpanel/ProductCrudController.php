cdb2b95c25b8debc0c5dec1f64e9cfdab1a2aab6
Pavel Klochkov <pv.klochkov@yandex.ru>
2024-03-27 21:14:29 +0300

    update ProductCrudController.php

--- src/Controller/Cpanel/ProductCrudController.php
+++ src/Controller/Cpanel/ProductCrudController.php
@@ -4,7 +4,7 @@ namespace App\Controller\Cpanel;
 
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +15,16 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield TextField::new('name');
+        yield TextField::new('code');
+        yield TextField::new('barcode');
+        yield TextEditorField::new('description');
+        yield ArrayField::new('characteristics');
+        yield ArrayField::new('photos');
+//        yield FieldCollection::new('productCategory');
     }
-    */
+
 }
